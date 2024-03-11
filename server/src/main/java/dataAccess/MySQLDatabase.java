56f6d255604e7b24aeb1aaedbe43dfb9c7bdd53e
Cj6701 <127358572+Cj6701@users.noreply.github.com>
2024-03-11 14:05:53 -0600

    Added the starter code for pregame

--- server/src/main/java/dataAccess/MySQLDatabase.java
+++ server/src/main/java/dataAccess/MySQLDatabase.java
@@ -358,7 +358,6 @@ public class MySQLDatabase implements DataAccess {
 
     private void clearTable(String table, int index) throws DataAccessException {
         var statement = String.format("DROP TABLE IF EXISTS %s",table);
-        System.out.println(createStatements[index]);
         try (var connection = DatabaseManager.getConnection()) {
             try (var preparedStatement = connection.prepareStatement(statement)) {
                 preparedStatement.executeUpdate();
